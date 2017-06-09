16007e76cf629edced0a826fa353d532c7d3c3d1
Jan Jurzitza <gh@webfreak.org>
2017-06-09 09:27:23 +0200

    Fixed mixed extern(C) and extern(Windows)

--- source/dwinrt/package.d
+++ source/dwinrt/package.d
@@ -64,30 +64,27 @@ enum winrtFactory(T) = WinrtFactory(winrtNameOf!T);
 
 extern (Windows)
 {
-	extern (C)
+	version (Win32)
 	{
-		version (Win32)
-		{
-			pragma(mangle, "GetRestrictedErrorInfo@4") HRESULT GetRestrictedErrorInfo(
-					IUnknown** info);
-			pragma(mangle, "RoGetActivationFactory@12") HRESULT RoGetActivationFactory(HSTRING classId,
-					const ref GUID iid, void** factory);
-			pragma(mangle, "RoInitialize@4") HRESULT RoInitialize(uint type);
-			pragma(mangle, "RoOriginateError@8") BOOL RoOriginateError(HRESULT error,
-					HSTRING message);
-			pragma(mangle, "RoUninitialize@0") void RoUninitialize();
-			pragma(mangle, "SetRestrictedErrorInfo@4") HRESULT SetRestrictedErrorInfo(
-					IUnknown* info);
-		}
-		else
-		{
-			HRESULT GetRestrictedErrorInfo(IUnknown** info);
-			HRESULT RoGetActivationFactory(HSTRING classId, const ref GUID iid, void** factory);
-			HRESULT RoInitialize(uint type);
-			BOOL RoOriginateError(HRESULT error, HSTRING message);
-			void RoUninitialize();
-			HRESULT SetRestrictedErrorInfo(IUnknown* info);
-		}
+		pragma(mangle, "GetRestrictedErrorInfo@4") HRESULT GetRestrictedErrorInfo(
+				IUnknown** info);
+		pragma(mangle, "RoGetActivationFactory@12") HRESULT RoGetActivationFactory(HSTRING classId,
+				const ref GUID iid, void** factory);
+		pragma(mangle, "RoInitialize@4") HRESULT RoInitialize(uint type);
+		pragma(mangle, "RoOriginateError@8") BOOL RoOriginateError(HRESULT error,
+				HSTRING message);
+		pragma(mangle, "RoUninitialize@0") void RoUninitialize();
+		pragma(mangle, "SetRestrictedErrorInfo@4") HRESULT SetRestrictedErrorInfo(
+				IUnknown* info);
+	}
+	else
+	{
+		HRESULT GetRestrictedErrorInfo(IUnknown** info);
+		HRESULT RoGetActivationFactory(HSTRING classId, const ref GUID iid, void** factory);
+		HRESULT RoInitialize(uint type);
+		BOOL RoOriginateError(HRESULT error, HSTRING message);
+		void RoUninitialize();
+		HRESULT SetRestrictedErrorInfo(IUnknown* info);
 	}
 }
 
--- source/dwinrt/winstring.d
+++ source/dwinrt/winstring.d
@@ -30,32 +30,29 @@ struct HSTRING_HEADER
 	ReservedT Reserved;
 }
 
-extern (C)
+extern (Windows)
 {
-	extern (Windows)
-	{
-		import core.sys.windows.windows;
+	import core.sys.windows.windows;
 
-	nothrow @nogc:
-		HRESULT WindowsCompareStringOrdinal(HSTRING str1, HSTRING str2, INT32* order);
-		HRESULT WindowsConcatString(HSTRING str1, HSTRING str2, HSTRING* out_);
-		HRESULT WindowsCreateString(LPCWSTR ptr, UINT32 len, HSTRING* out_);
-		HRESULT WindowsCreateStringReference(LPCWSTR ptr, UINT32 len,
-				HSTRING_HEADER* header, HSTRING* out_);
-		HRESULT WindowsDeleteString(HSTRING str);
-		HRESULT WindowsDeleteStringBuffer(HSTRING_BUFFER buf);
-		HRESULT WindowsDuplicateString(HSTRING str, HSTRING* out_);
-		UINT32 WindowsGetStringLen(HSTRING str);
-		LPCWSTR WindowsGetStringRawBuffer(HSTRING str, UINT32* len);
-		BOOL WindowsIsStringEmpty(HSTRING str);
-		HRESULT WindowsPreallocateStringBuffer(UINT32 len, WCHAR** outptr, HSTRING_BUFFER* out_);
-		HRESULT WindowsPromoteStringBuffer(HSTRING_BUFFER buf, HSTRING* out_);
-		HRESULT WindowsReplaceString(HSTRING haystack, HSTRING needle,
-				HSTRING replacement, HSTRING* out_);
-		HRESULT WindowsStringHasEmbeddedNull(HSTRING str, BOOL* out_);
-		HRESULT WindowsSubstring(HSTRING str, UINT32 pos, HSTRING* out_);
-		HRESULT WindowsSubstringWithSpecifiedLength(HSTRING str, UINT32 pos, UINT32 len, HSTRING* out_);
-		HRESULT WindowsTrimStringEnd(HSTRING str, HSTRING charstr, HSTRING* out_);
-		HRESULT WindowsTrimStringStart(HSTRING str, HSTRING charstr, HSTRING* out_);
-	}
+nothrow @nogc:
+	HRESULT WindowsCompareStringOrdinal(HSTRING str1, HSTRING str2, INT32* order);
+	HRESULT WindowsConcatString(HSTRING str1, HSTRING str2, HSTRING* out_);
+	HRESULT WindowsCreateString(LPCWSTR ptr, UINT32 len, HSTRING* out_);
+	HRESULT WindowsCreateStringReference(LPCWSTR ptr, UINT32 len,
+			HSTRING_HEADER* header, HSTRING* out_);
+	HRESULT WindowsDeleteString(HSTRING str);
+	HRESULT WindowsDeleteStringBuffer(HSTRING_BUFFER buf);
+	HRESULT WindowsDuplicateString(HSTRING str, HSTRING* out_);
+	UINT32 WindowsGetStringLen(HSTRING str);
+	LPCWSTR WindowsGetStringRawBuffer(HSTRING str, UINT32* len);
+	BOOL WindowsIsStringEmpty(HSTRING str);
+	HRESULT WindowsPreallocateStringBuffer(UINT32 len, WCHAR** outptr, HSTRING_BUFFER* out_);
+	HRESULT WindowsPromoteStringBuffer(HSTRING_BUFFER buf, HSTRING* out_);
+	HRESULT WindowsReplaceString(HSTRING haystack, HSTRING needle,
+			HSTRING replacement, HSTRING* out_);
+	HRESULT WindowsStringHasEmbeddedNull(HSTRING str, BOOL* out_);
+	HRESULT WindowsSubstring(HSTRING str, UINT32 pos, HSTRING* out_);
+	HRESULT WindowsSubstringWithSpecifiedLength(HSTRING str, UINT32 pos, UINT32 len, HSTRING* out_);
+	HRESULT WindowsTrimStringEnd(HSTRING str, HSTRING charstr, HSTRING* out_);
+	HRESULT WindowsTrimStringStart(HSTRING str, HSTRING charstr, HSTRING* out_);
 }
